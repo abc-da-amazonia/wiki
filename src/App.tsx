834eadd1e498b18ce1c09ed0f2b5aaf9c493d7cb
alanmarinho <a13br1313@gmail.com>
2026-07-11 11:22:02 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hello from '@components/hello';
+
+function App() {
+  return (
+    <div className="flex flex-1 min-h-screen items-center justify-center text-red-600">
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
